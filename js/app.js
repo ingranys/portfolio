e7debf917854491541eda190edde1f3483031cb6
ingranys <ingranys@protonmail.com>
2021-04-25 04:37:45 -0400

    Add dynamic settings for particles and clean code

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
-/* toggle menu */
+/*
+#######################################################
+TOGGLE
+#######################################################
+*/
+/* get elements */
 const toggle = document.querySelector('.toggle');
 const menu = document.querySelector('.menu');
 const header = document.querySelector('.header');
 
+/* toggle on click */
 toggle.addEventListener('click', () => {
   menu.classList.toggle('active');
   header.classList.toggle('active');
 })
 
 
+/*
+#######################################################
+SVG
+#######################################################
+*/
 /* convert svg files to inline html */
+/* hide img html elements until they are converted > style="display:none;"*/
 const convertImages = (query, callback) => {
     const images = document.querySelectorAll(query);
   
@@ -18,9 +30,10 @@ const convertImages = (query, callback) => {
       .then(res => res.text())
       .then(data => {
         const parser = new DOMParser();
+        /* fetch raw svg from file */
         const svg = parser.parseFromString(data, 'image/svg+xml').querySelector('svg');
   
-
+        /* retrive attributes*/
         if (image.id) svg.id = image.id;
         if (image.className) svg.classList = image.classList;
         if (image.title) {
@@ -30,11 +43,13 @@ const convertImages = (query, callback) => {
         }
 
         if (image.getAttribute('link')){
+          /* put inside an anchor whe link is required and replace image */
           var link = document.createElement('a');
           link.setAttribute('href', image.getAttribute('link'));
           link.appendChild(svg);  
           image.parentNode.replaceChild(link, image);  
         } else {
+          /* replace image with generated svg */ 
           image.parentNode.replaceChild(svg, image);
         }
         
@@ -47,23 +62,74 @@ const convertImages = (query, callback) => {
 convertImages('img');
 
 
-/* particles.JS.load(@dom-id, @path-json, @callback (optional)); */
+/*
+#######################################################
+PARTICLES
+#######################################################
+*/
+/* load configuration file, fine-tune params and start */
+/* setParticles(@dom-id, @path-json, @callback (optional)); */
+setParticles = function(tag_id, path_config_json, callback){
+
+  /* load json config */
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', path_config_json);
+  xhr.onreadystatechange = function (data) {
+    if(xhr.readyState == 4){
+      if(xhr.status == 200){
+        var params = JSON.parse(data.currentTarget.response);
+        params = adaptParticlesParams(tag_id,params);
+        window.particlesJS(tag_id, params);
+        if(callback) callback();
+      }else{
+        console.log('Error pJS - XMLHttpRequest status: '+xhr.status);
+        console.log('Error pJS - File config not found');
+      }
+    }
+  };
+  xhr.send();
+};
+
+/* adaptParticlesParams(@dom-id, @path-json); */
+adaptParticlesParams = function(tag_id, params){    
+  const particlesElement = document.querySelector('#'+tag_id);
+  var size = Math.min(particlesElement.clientWidth,particlesElement.clientHeight);
+  /*device pixel ratio can mess with radius value*/
+  /*var pixelRatio = window.devicePixelRatio;*/
+  /*var repulseDistance = 0.5 * 0.8 * size * pixelRatio ;
+
+  /* adjust repulse radius to canvas size */
+  var repulseDistance = 0.5 * 0.8 * size ;
+  params.interactivity.modes.repulse.distance = repulseDistance;
+
+  return params;
+};
+
+/* reload particles when screen size is updated */
 var loadParticles = function(event) {
-  if (window.innerWidth > 600)  {
-    particlesJS.load('particles', 'config/particles-large.json', function() {
-      console.log('Particles.js configuration loaded.');
-    });  
-  } else {
-    particlesJS.load('particles', 'config/particles-small.json', function() {
-      console.log('Particles.js configuration loaded.');
-    }); 
-  }
+  setParticles('particles', 'config/particles.json', function() {
+    console.log('Particles.js configuration loaded.');
+  });
 };  
 
 window.onresize = loadParticles;
 window.onload = loadParticles;
 
+/* load particles from configuration file */
+/* particles.JS.load(@dom-id, @path-json, @callback (optional)); */
+/*
+    particlesJS.load('particles', 'config/particles.json', function() {
+      console.log('Particles.js configuration loaded.');
+});
+*/
 
+
+
+/*
+#######################################################
+TYPED
+#######################################################
+*/
 /* typed.js */
 var typedOptions = document.getElementById('typed-options');
 var elementTypeSpeed = typedOptions.getAttribute('type-speed');
